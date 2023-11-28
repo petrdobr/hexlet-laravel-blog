406ec0c6b39817c0acbe8a54198fc6913409afcb
petrdobr <dobrokhotovp@gmail.com>
2023-11-28 17:15:27 +0400

    lesson 12

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2>{{$article->name}}</h2>
+        <h2><a href="{{ route('articles.show', $article->id) }}">
+            {{$article->name}}
+        </a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
     {{ $articles->links() }}
