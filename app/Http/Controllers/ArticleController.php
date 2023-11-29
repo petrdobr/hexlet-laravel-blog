f3644145ab32fe7265b1b9080601a1f7ef05d257
petrdobr <dobrokhotovp@gmail.com>
2023-11-29 12:30:48 +0400

    lesson 14

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,7 +10,7 @@ class ArticleController extends Controller
     public function index()
     {
 
-    $articles = Article::paginate(2);
+    $articles = Article::paginate(5);
     // Статьи передаются в шаблон
     // compact('articles') => [ 'articles' => $articles ]
     return view('article.index', compact('articles'));
@@ -20,5 +20,25 @@ class ArticleController extends Controller
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+        'name' => 'required|unique:articles',
+        'body' => 'required|min:1000'
+        ]);
+
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+
+        return redirect()->route('articles.index');
     }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    protected $fillable = ['name', 'body'];
     use HasFactory;
 }
